4b9594f06dca90bcf9c0f65983cb5a1609ed0152
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-05-26 23:36:43 -0300

    prova

---
+++ static/js/script.js
@@ -0,0 +1,73 @@
+
+// fazendo funcionar o relogio
+var r = document.getElementById('relogio');
+
+function atualizaHora(){
+    var hoje = new Date();
+    var h = hoje.getHours();
+    var m = hoje.getMinutes();
+    var s = hoje.getSeconds();
+    if(h < 10)
+        h = '0' + h;
+    if(m < 10)
+        m = '0' + m;
+    if(s < 10)
+        s = '0' + s;
+    r.textContent = h + ":" + m + ":" + s;
+}
+
+setInterval(atualizaHora, 1000); 
+
+
+const raiz = document.getElementById('lista');
+
+
+
+fetch('/funcionarios') // GET por default
+.then(res => {return res.json();})
+.then(data => {
+    data.funcionarios.forEach(element => {
+        // criamos a linha da tabela
+        var linha = document.createElement('tr');
+        raiz.appendChild(linha);
+
+        // criamos as colunas
+        var funcionario = document.createElement('td');
+        funcionario.textContent = element.nome_usuario;
+        linha.appendChild(funcionario);
+
+        var hora = document.createElement('td');
+        hora.textContent = element.hora;
+        linha.appendChild(hora);
+    });
+})
+.catch( err => {
+    console.log('Ocorreu um problema.');
+})
+.finally(() =>{
+    console.log('Linha que sempre aparece no final');
+})
+
+
+const app =document.getElementById('raiz')
+
+
+const caixa = document.createElement('div');
+
+caixa.setAttribute('class','caixa');
+app.appendChild(caixa);
+
+for(var i = 1; i <= 10; i++){
+    var c = document.createElement('div');
+    c.setAttribute('class','artigos')
+    caixa.appendChild(c);
+
+    var t = document.createElement('h3');
+    t.textContent = 'Curso ' + i;
+    c.appendChild(t);
+
+    var p = document.createElement('p');
+    p.textContent = 'The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa.';
+    c.appendChild(p);
+}
+
